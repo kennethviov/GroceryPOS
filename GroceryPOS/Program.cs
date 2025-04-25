38b718c915f6abc25cdb13ffb51234e5ed0a79db
Jam <jamardines16@gmail.com>
2025-04-25 10:33:42 +0800

    nigro

--- GroceryPOS/Program.cs
+++ GroceryPOS/Program.cs
@@ -16,7 +16,7 @@ namespace GroceryPOS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new loginForm());
+            Application.Run(new Inventory());
 
         }
     }
